4d9320ffbfe354c5a3cba2f2235cfb6242ba89f9
Marc Smith <smivers624@gmail.com>
2016-04-14 15:42:27 +0800

    Fix: Contact and flyer pages, new nav item.

---
+++ earlyApril16/contact/mail_settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$email_to = "admin@marcsmith.website"; // your email address send TO
+$email_from = "admin@marcsmith.website"; // your email address send FROM
+$email_subject = "2Hfd Contact Form Enquiry!";
+$thankyou = "contactthanks.html#thank-you";
+
+?>
